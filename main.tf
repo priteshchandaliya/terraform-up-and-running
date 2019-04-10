470cb621ace1ab2783e4a346c3a3a2396929f92a
priteshchandaliya <priteshchandaliya@gmail.com>
2019-04-10 13:47:18 -0700

    add a security group and assign to the aws instance. Also run a script on the base OS using terraform

--- main.tf
+++ main.tf
@@ -6,9 +6,28 @@ provider "aws" {
 resource "aws_instance" "single_webserver" {
     ami = "ami-40d28157"
     instance_type = "t2.micro"
+    vpc_security_group_ids = ["${aws_security_group.instance.id}"]
 
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, Pritesh" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
     tags {
     Name = "terraform_up_and_running_example_1"
     }
 }
+
+resource "aws_security_group" "instance" {
+    name = "single_webserver_instance"
+
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+
 
